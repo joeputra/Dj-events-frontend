bb60f417d540602b15a6a4802382deedfedeb43b
joeputra <joe.putra40@gmail.com>
2021-06-21 10:59:45 +0700

    Layout, Header, Footer, Router

---
+++ components/Layout.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Header from './Header'
+import Footer from './Footer'
+import styles from '../styles/Layout.module.css'
+
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={description} />
+        <meta name='keywords' content={keywords} />
+      </Head>
+      <Header />
+      <div className={styles.container}>{children}</div>
+      <Footer />
+    </div>
+  )
+}
+
+Layout.defaultProps = {
+  title: 'Games Events | Find the hottest Game',
+  description: 'Find the lates Games and More',
+  keywords: 'Moba, FPS, Survival, Puzzel, More',
+}
